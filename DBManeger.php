7891ac22d741fc0a6c80666b9e329aedcff28f4f
tarotarotarot <tarotou1018@gmail.com>
2023-02-11 02:43:11 +0900

    絞り込みテスト

--- DBManeger.php
+++ DBManeger.php
@@ -7,7 +7,8 @@ class DBManager
         return $pdo;
     }
 
-    function disply_areas(){
+    function disply_areas()
+    {//エリア全表示
         $pdo = $this->dbconnect();
         $sql = "SELECT * FROM areas";
         $ans=$pdo->query($sql);
@@ -15,6 +16,41 @@ class DBManager
         foreach($ans as $row){
             echo $row["area_name"]."<br>";
         }
+    }
+
+    function search_prefectures($label_data)
+    {//ラベルによる絞り込み
+        $pdo = $this->dbconnect();
+        $data_num = count($label_data);
+        $sql = "SELECT * FROM prefectures WHERE";
+        for($i=0;$i<$data_num;$i++){
+            if($i==0){
+                $sql = $sql . "label LIKE '?' ";
+                continue;
+            }
+            $sql = $sql . "AND label LIKE '?' ";
+        }
+        $sql = $sql.";";
+
+        $ps = $pdo->prepare($sql);
+
+        for ($i = 0; $i < $data_num; $i++) {
+            $ps->bindValue($i + 1,"%".$label_data[$i]."%", PDO::PARAM_STR);
+        }
+
+        $ps->execute();
+
+        $search = $ps->fetchAll();
+
+        if (!isset($search)) {
+            return 0;
+        }
+
+        foreach($search as $after){
+            echo $after["prefectures_name"];
+        }
+
+        
     }
 }
 ?>
--- outputTest.php
+++ outputTest.php
@@ -1,16 +1,9 @@
 <?php
+    require "DBManeger.php";
     $ans = $_POST["label"];
-
-    $pdo = new PDO('mysql:host=localhost;dbname=nekodb;charset=utf8',
-        'root','example_pass');
-    $con_num = count($ans);
-    $sql="SELECT prefectures_name FROM prefectures WHERE";
-    for($i=0;$i<$con_num;$i++){
-        if(){
-
-        }
-    }
-        $ans=$pdo->query($sql);
     
     print_r($ans);
+
+    $dbm = new DBManager();
+    $dbm->search_prefectures($ans);
 ?>
