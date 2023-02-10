a09ac679f9c83ede7f15a0c33b0dfc0fc8f9cff7
nekoy3 <nekoyama3.n@gmail.com>
2023-02-11 00:39:06 +0900

    db example php

---
+++ example.php
@@ -0,0 +1,43 @@
+!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>タイトル</title>
+</head>
+<body>
+<?php
+try{
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=studydb;charset=utf8',
+        'testuser',
+        'testpass'
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+try{
+    $sql = "SELECT * FROM studydb.items";
+    $stmh = $pdo->prepare($sql);
+    $stmh->execute();
+}catch(PDOException $Exception){
+    die('接続エラー：' .$Exception->getMessage());
+}
+?>
+<table><tbody>
+    <tr><th>ID</th><th>商品名</th></tr>
+<?php
+    while($row = $stmh->fetch(PDO::FETCH_ASSOC)){
+?>
+    <tr>
+        <th><?=htmlspecialchars($row['id'])?></th>
+        <th><?=htmlspecialchars($row['name'])?></th>
+    </tr>
+<?php
+    }
+    $pdo = null;
+?>
+</tbody></table>
+</body>
+</html>
