f069864083056e36745247fed3efdcc6cee3cee8
tarotarotarot <tarotou1018@gmail.com>
2023-02-11 18:25:42 +0900

    セレクトボックスの値取得

--- TaroTest/script/resultScript.js
+++ TaroTest/script/resultScript.js
@@ -5,7 +5,7 @@ const vm = new Vue({
         return{
             areas_data: [{ area_id: 0, area_name: ""}],
             prefectures_data: [{ prefectures_id: "00", area_id: "00", prefectures_name: "", longitube: 0, longitube: 0, label: "", charm_rank: 0 }],
-            area_value: '0',
+            area_value: ['0'],
         };
     },
     //ページが読み込まれた時に動く処理
