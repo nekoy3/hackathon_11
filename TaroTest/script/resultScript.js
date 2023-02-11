d766dfe62c7bd907b530f0504d8d8b6c9723a224
tarotarotarot <tarotou1018@gmail.com>
2023-02-11 14:23:56 +0900

    都道府県情報取得・表示

---
+++ TaroTest/script/resultScript.js
@@ -0,0 +1,28 @@
+const vm = new Vue({
+    el: '#app',
+    data: {
+        //初期値
+        results: { id: 00, name: "OK" },
+        prefectures_data: { prefectures_id: "00", area_id: "00", prefectures_name: "", longitube: 0, longitube: 0, label: "", charm_rank: 0 }
+    },
+    //ページが読み込まれた時に動く処理
+    mounted() {
+        axios
+            //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
+            .get("phpFile/test.php")
+            .then((response) => (this.results = response.data))
+            .catch((error) => console.log(error));
+
+        axios
+            //timestamp=${new Date().getTime()}を入れることで毎回違うアドレスで検索が出来るから以前のキャッシュを読み込まない
+            .get("phpFile/prefecturesDisply.php")
+            .then((response) => (this.prefectures_data = response.data))
+            .catch((error) => console.log(error));
+    },
+    methods: {
+    },
+
+    computed: {
+    }
+
+});
