297a8423ad8e2f3828faa055a18ff5cb6680850b
tarotarotarot <tarotou1018@gmail.com>
2023-02-12 05:31:17 +0900

    セッションテスト

--- scripts/Vuejs/resultScript.js
+++ scripts/Vuejs/resultScript.js
@@ -6,7 +6,7 @@ const vm = new Vue({
             areas_data: [{ area_id: 0, area_name: ""}],
             prefectures_data: [{ prefectures_id: "00", area_id: "00", prefectures_name: "", longitube: 0, longitube: 0, label: "", charm_rank: 0 ,prefectures_button_flg:true}],
             area_value: 0,
-            checked_prefectures: [],
+            checked_prefectures: [{key:"00",value:""}],
         };
     },
     //ページが読み込まれた時に動く処理
@@ -34,24 +34,15 @@ const vm = new Vue({
             const id=this.prefectures_data[i].prefectures_id;
             const pName=this.prefectures_data[i].prefectures_name;
             this.checked_prefectures.push({key:id,value:pName});
+            sessionStorage.setItem("prefectures_array", JSON.stringify(this.checked_prefectures));
         },
         delete_prefectures(i){
             this.prefectures_data[i].prefectures_button_flg = !this.prefectures_data[i].prefectures_button_flg;
             alert("クリック");
-        },
-        // 画面遷移直前に実行するメソッド
-        beforeRouteLeave(to, from, next) {
-            // 画面遷移前に必要な処理を実行する
-            sessionStorage.setItem("prefectures_array", JSON.stringify(this.checked_prefectures));
-
-            // 画面遷移を続けるために必要な処理
-            next();
         }
     },
+
     computed: {
-    },
-    beforeRouteLeave: {
-        beforeRouteLeave
-    },
+    }
 
 });
