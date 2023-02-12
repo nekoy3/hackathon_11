fa299cb36e227bff686ab97499749f1c554e6c4e
nekoy3 <nekoyama3.n@gmail.com>
2023-02-12 15:05:42 +0900

    ルーレット(の残骸）

---
+++ roulette.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      items: JSON.parse(sessionStorage.getItem("roulette-items")),
+      rotateDeg: 0
+    };
+  },
+  methods: {
+    startRoulette() {
+      const randDeg = Math.floor(Math.random() * 360);
+      this.rotateDeg = randDeg;
+      this.$nextTick(() => {
+        const totalDeg = 360 * 10 + randDeg;
+        this.rotateDeg = totalDeg;
+        setTimeout(() => {
+          this.rotateDeg = totalDeg - (totalDeg % 360);
+        }, 2000);
+      });
+    }
+  }
+};
+</script>
