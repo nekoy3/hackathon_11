1155b8ae53a3950facf249bbe0eb9940bc087918
tarotarotarot <tarotou1018@gmail.com>
2023-02-12 14:07:40 +0900

    乱数結果

--- scripts/Vuejs/resultScript.js
+++ scripts/Vuejs/resultScript.js
@@ -2,13 +2,13 @@ const vm = new Vue({
     el: '#app',
     data() {
         //初期値
-        return{
-            areas_data: [{ area_id: 0, area_name: ""}],
-            prefectures_data: [{ prefectures_id: "00", area_id: "00", prefectures_name: "", longitube: 0, longitube: 0, label: "", charm_rank: 0 ,prefectures_button_flg:true}],
+        return {
+            areas_data: [{ area_id: 0, area_name: "" }],
+            prefectures_data: [{ prefectures_id: "00", area_id: "00", prefectures_name: "", longitube: 0, longitube: 0, label: "", charm_rank: 0, prefectures_button_flg: true }],
             area_value: 0,
             checked_prefectures: [],
-            selected_list:[],
-            result:"",
+            selected_list: [],
+            result: "",
         };
     },
     //ページが読み込まれた時に動く処理
@@ -28,15 +28,15 @@ const vm = new Vue({
         Promise.all([
             axios.get("https://nekoy3.net/hackathon_11/scripts/php/areaDisplay.php"),
             axios.get("https://nekoy3.net/hackathon_11/scripts/php/prefecturesDisplay.php")
-          ]).then(([areasData, prefecturesData]) => {
+        ]).then(([areasData, prefecturesData]) => {
             this.areas_data = areasData.data;
             this.prefectures_data = prefecturesData.data;
-          }).catch((error) => console.log(error));
+        }).catch((error) => console.log(error));
 
         var value = sessionStorage.getItem("result");
-        this.result=JSON.parse(value);
+        this.result = JSON.parse(value);
 
-        if(sessionStorage.getItem('selectData') !== null){
+        if (sessionStorage.getItem('selectData') !== null) {
             const pushed = sessionStorage.getItem('selectData');
             console.log(pushed);
             const pushedArray = JSON.parse(pushed);
@@ -45,16 +45,16 @@ const vm = new Vue({
             console.log(pushedArray);
             for (let i = 0; i < this.selected_list.length; i++) {
                 for (let j = 0; j < this.prefectures_data.length; j++) {
-                  if (this.selected_list[i].key === this.prefectures_data[j].prefectures_id) {
-                    this.prefectures_data[j].prefectures_button_flg = !this.prefectures_data[j].prefectures_button_flg;
-                    const id=this.prefectures_data[i].prefectures_id;
-                    const pName=this.prefectures_data[i].prefectures_name;
-                    this.checked_prefectures.push({key:id,value:pName});
-                    break;
-                  }
+                    if (this.selected_list[i].key === this.prefectures_data[j].prefectures_id) {
+                        this.prefectures_data[j].prefectures_button_flg = !this.prefectures_data[j].prefectures_button_flg;
+                        const id = this.prefectures_data[i].prefectures_id;
+                        const pName = this.prefectures_data[i].prefectures_name;
+                        this.checked_prefectures.push({ key: id, value: pName });
+                        break;
+                    }
                 }
             }
-        }else{
+        } else {
             console.log("検知失敗");
         }
 
@@ -64,27 +64,30 @@ const vm = new Vue({
             this.area_value = e.target.value;
             console.log(e.target.value);
         },
-        add_prefectures(i){
+        add_prefectures(i) {
             this.prefectures_data[i].prefectures_button_flg = !this.prefectures_data[i].prefectures_button_flg;
-            const id=this.prefectures_data[i].prefectures_id;
-            const pName=this.prefectures_data[i].prefectures_name;
-            this.checked_prefectures.push({key:id,value:pName});
+            const id = this.prefectures_data[i].prefectures_id;
+            const pName = this.prefectures_data[i].prefectures_name;
+            this.checked_prefectures.push({ key: id, value: pName });
         },
-        delete_prefectures(i){
+        delete_prefectures(i) {
             this.prefectures_data[i].prefectures_button_flg = !this.prefectures_data[i].prefectures_button_flg;
             alert("クリック");
         },
-        onClickSession(){
+        onClickSession() {
             // console.log(JSON.stringify(this.checked_prefectures));
             const selectedData = JSON.stringify(this.checked_prefectures);
             sessionStorage.setItem('selectData', selectedData);
-            location.href="https://nekoy3.net/hackathon_11/resultList.html";
+            location.href = "https://nekoy3.net/hackathon_11/resultList.html";
         },
-        randamValue(len){
-            let r = Math.random() * ( len - 1 ) + len;
-            const resultPrefectures = JSON.stringify(this.selected_list[r].value);
-            sessionStorage.setItem("result",resultPrefectures);
-            location.href="https://nekoy3.net/hackathon_11/result.html";
+        randamValue(len) {
+            let r = Math.random() * (len - 1) + len;
+            // const resultPrefectures = JSON.stringify(this.selected_list[r].value);
+            // sessionStorage.setItem("result",resultPrefectures);
+            const arrayed_selected_list = JSON.stringify(this.selected_list);
+            const resultPrefectObject = arrayed_selected_list[r].value;
+            sessionStorage.setItem("result", resultPrefectObject);
+            location.href = "https://nekoy3.net/hackathon_11/result.html";
         },
     },
 
